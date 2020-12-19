081a8c3c5bf3cab22af648ac4feb7b3650dce93b
Adam Jedlička <adajedlicka@gmail.com>
2020-12-19 17:54:20 +0100

    SolidJS 0.23.4

--- packages/liquid-demo/repositories/index.js
+++ packages/liquid-demo/repositories/index.js
@@ -1,18 +1,14 @@
-import { createEffect, createResource } from 'solid-js'
-import { isServer } from 'solid-js/web'
+import { createComputed, createResource } from 'solid-js'
 
 export const useRepository = (name, repository, args = []) => {
   const [resource, loadResource] = createResource(undefined, { name })
 
-  if (isServer) {
-    loadResource(repository)
-  } else {
-    createEffect(() => {
-      for (const arg of args) arg()
+  createComputed(() => {
+    // Register the arguments
+    for (const arg of args) arg()
 
-      loadResource(repository)
-    })
-  }
+    loadResource(repository)
+  })
 
   return resource
 }
