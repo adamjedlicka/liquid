07571af4e6a566a8213520df4d06c4ee89535bd3
Adam Jedlička <adajedlicka@gmail.com>
2020-12-19 19:16:30 +0100

    Remove support for _.js

--- packages/liquid-demo/routes/graphql.js
+++ packages/liquid-demo/routes/graphql.js
@@ -1,6 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = createProxyMiddleware({
-  target: 'https://venia.magento.com/graphql',
+  target: 'https://venia.magento.com',
   changeOrigin: true,
 })
