a8d5dfd8a038bb728a8634f15030e24abfe36970
Adam Jedlička <adajedlicka@gmail.com>
2020-12-22 15:13:02 +0100

    Fixes

--- packages/liquid-demo/mappers/ProductMapper.js
+++ packages/liquid-demo/mappers/ProductMapper.js
@@ -1,8 +1,10 @@
+import { toCategory } from './CategoryMapper'
+
 export const toProduct = (data) => ({
   name: data.name ?? '',
   urlPath: '/' + (data.url_key ?? ''),
   thumbnail: data.thumbnail?.url ?? '',
-  description: data.description?.url ?? '',
+  description: data.description?.html ?? '',
   finalPrice: data.price_range?.minimum_price?.final_price?.value ?? 0,
-  categories: data.categories ?? [],
+  categories: (data.categories ?? []).map(toCategory),
 })
