6d360d4ffbe2381e0c8ab8f48a4a2ba785be7875
Adam Jedlička <adajedlicka@gmail.com>
2021-01-03 19:09:53 +0100

    Add Head.js

--- packages/liquid-demo/dynamicPages/CATEGORY.js
+++ packages/liquid-demo/dynamicPages/CATEGORY.js
@@ -1,8 +1,9 @@
-import { createMemo, For } from 'solid-js'
-import { useRouter, Head } from 'liquid-js'
+import { createMemo, createSignal, For, Show } from 'solid-js'
+import { useRouter } from 'liquid-js'
 import { fetchCategoryById } from '../repositories/CategoryRepository'
 import Tile from '../components/product/Tile'
 import Pagination from '../components/Pagination'
+import { Portal } from 'solid-js/web'
 
 export default (props) => {
   const { query } = useRouter()
@@ -13,11 +14,18 @@ export default (props) => {
 
   const pages = createMemo(() => Math.ceil(category.productsTotal / 20))
 
+  const [show, setShow] = createSignal(false)
+
   return (
     <>
-      <Head>
-        <title>{category.category.name}</title>
-      </Head>
+      <Show when={show()}>
+        <div>Is visible</div>
+        <Portal mount={document.head}>
+          <title>Hello, World!</title>
+        </Portal>
+      </Show>
+
+      <button onClick={() => setShow(!show())}>Click me!</button>
 
       <section class="text-gray-700 body-font">
         <div class="container px-5 py-24 mx-auto">
